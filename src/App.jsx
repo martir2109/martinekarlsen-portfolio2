085b2d01fec11826d5e479f788594a98c00f5cb4
Martine R Karlsen <martinekar08@gmail.com>
2026-05-31 16:15:08 +0200

    Add routes to all the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/shared/Layout";
+import Home from "./pages/Home";
+import CSSFrameworks from "./pages/CSSFramworks";
+import JavaScriptFrameworks from "./pages/JavaScriptFrameworks";
+import SemesterProject2 from "./pages/SemesterProject2";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+
 function App() {
   return (
-    <>
-      <h1>.</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="cssframeworks" element={<CSSFrameworks />} />
+          <Route
+            path="javascriptframeworks"
+            element={<JavaScriptFrameworks />}
+          />
+          <Route path="semesterproject2" element={<SemesterProject2 />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
