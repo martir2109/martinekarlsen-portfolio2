33fe94a78cab6105973c6638e699646007a9b534
Martine R Karlsen <martinekar08@gmail.com>
2026-05-31 16:42:14 +0200

    Add projects data

---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+const projects = [
+  {
+    id: 1,
+    path: "cssframeworks",
+    title: "CSS Frameworks",
+    description:
+      "A full-featured social media application where users can create, edit, and delete posts, follow and unfollow others, leave comments, and react to content.",
+    tag: "Frontend development",
+    image: "/JS2Preview.png",
+  },
+  {
+    id: 2,
+    path: "javascriptframeworks",
+    title: "JavaScript Frameworks",
+    description:
+      "A fully functional e-commerce store built with React and Next.A fully functional e-commerce store built with React and Next.js, featuring product listings, search and sorting, detailed product pages, a shopping cart, checkout flow, a contact form, and full mobile responsiveness.",
+    tag: "Frontend development",
+    image: "/JSFWPreview.png",
+  },
+  {
+    id: 3,
+    path: "semesterproject2",
+    title: "Semester project 2",
+    description:
+      "An auction house web application built on Noroff's API, where registered users can manage profiles, create and bid on listings, and track bid history, while guests can browse and search without an account. Built with a mobile-first, responsive design using full CRUD functionality.",
+    tag: "Frontend development",
+    image: "/SP2Preview.png",
+  },
+];
+
+export default projects;
