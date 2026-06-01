293990689aaa3e1ad47a1ab35a45a0d10c2446ee
Martine R Karlsen <martinekar08@gmail.com>
2026-06-01 19:26:54 +0200

    Create ScrollToTop component
    - Scrolls the page to the top whenever the user navigates to a new page.

---
+++ src/components/shared/ScrollToTop.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+/**
+ * Scrolls the page to the top whenever the user navigates to a new page.
+ */
+export default function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
